e4b96902db4b38168c0341a3369f6cacfa4931bc
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-09-10 23:34:33 -0700

    First Commit

---
+++ movie-website-clone/components/navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+	return{
+		<div className="logo">
+			<img src="img/Logo.png" alt="" />
+			<h3>MovieBox</h3>
+		</div>
+		
+		<form action="">
+			<input type="search" name="" id="" />
+		</form>
+
+		<div className="signIn">
+
+		</div>
+
+	}
+} 
+
+export default Navbar
